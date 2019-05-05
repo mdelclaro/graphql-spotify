e4b8f34bac05724d9bd6aaa1d1b35acc20651ebe
Matheus Del Claro <matheusdelclaro@gmail.com>
2019-05-05 15:38:12 -0300

    added users playlist endpoint

--- src/graphql/resolvers/playlist.js
+++ src/graphql/resolvers/playlist.js
@@ -37,5 +37,42 @@ module.exports = {
       console.log(err);
       return err.message;
     }
+  },
+
+  userPlaylists: (args, req) => {
+    try {
+      return new Promise((resolve, reject) => {
+        if (!req.isAuth) {
+          throw new Error(
+            "This endpoint requires authentication. Go to /signin to authenticate."
+          );
+        }
+
+        const options = {
+          url: spotify_base_url + "/users/" + args.userId + "/playlists",
+          headers: {
+            Authorization: "Bearer " + req.token
+          }
+        };
+
+        request.get(options, (error, response, body) => {
+          if (error) {
+            console.log(error);
+            throw new Error(error);
+          }
+          result = JSON.parse(body);
+          let playlists = [];
+
+          result.items.map(item => {
+            playlists.push(item);
+          });
+
+          resolve(playlists);
+        });
+      });
+    } catch (err) {
+      console.log(err);
+      return err.message;
+    }
   }
 };
